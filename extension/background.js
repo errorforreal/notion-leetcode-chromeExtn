1ba857f91bed07c1269505c029ab84170def25a5
Krish Yadav <krishyadav2806@gmail.com>
2026-04-11 23:30:11 +0530

    extension completed

--- extension/background.js
+++ extension/background.js
@@ -1,14 +1,28 @@
 chrome.runtime.onMessage.addListener((message, sender) => {
     if (message.type === "OPEN_POPUP") {
+  
       chrome.storage.local.set({
         scrapedData: message.payload
       });
   
-      chrome.windows.create({
-        url: "popup.html",
-        type: "popup",
-        width: 400,
-        height: 500
+      // get screen size
+      chrome.system.display.getInfo((displays) => {
+        const screen = displays[0].workArea;
+  
+        const width = 360;
+        const height = 500;
+  
+        const left = screen.width - width - 20; // 20px margin
+        const top = 20;
+  
+        chrome.windows.create({
+          url: "popup.html",
+          type: "popup",
+          width,
+          height,
+          left,
+          top
+        });
       });
     }
   });
